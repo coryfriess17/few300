62602aa05fd06c583988afc86b30fa8a22a26a6e
coryfriess17 <cfriess17@jcu.edu>
2019-07-10 19:37:42 +0000

    end of day one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FourOhFourComponent } from './components/four-oh-four/four-oh-four.component';
+import { HomeComponent } from './components/home/home.component';
+import { MathComponent } from './features/math/math.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'not-found',
+    component: FourOhFourComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
